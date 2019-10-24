d848c4fc99b7e1f933f002d14a6355bf118fb4a1
Spirit0005 <m_hamzak1997@yahoo.com>
2019-10-24 14:27:37 +0500

    Addition done

---
+++ HomeChef/Home/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  HomeChef
+//
+//  Created by 2 Minut on 10/24/19.
+//  Copyright © 2019 Hamza . All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
